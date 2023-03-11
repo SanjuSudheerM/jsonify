121eb231b2e364d335f532326987d1f16b912649
Sanju <sanjusdhr@gmail.com>
2023-03-11 20:17:32 +0530

    Added tab list

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,62 @@
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
-import { JsonTab } from "./components/Tab";
-import { JSONEditor } from "./components/Editor";
-import { DBConfig } from "./config/DBConfig";
-import { initDB } from "react-indexed-db";
-import { ErrorBoundary } from "./ErrorBoundary";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { TabNameDialog } from "./components/TabNameDialog";
-import { useState } from "react";
+import {JsonTab} from "./components/Tab";
+import {JSONEditor} from "./components/Editor";
+import {DBConfig} from "./config/DBConfig";
+import {initDB} from "react-indexed-db";
+import {ErrorBoundary} from "./ErrorBoundary";
+import {Header} from "./components/Header";
+import {Footer} from "./components/Footer";
+import {TabNameDialog} from "./components/TabNameDialog";
+import {useState} from "react";
+import Drawer from '@mui/material/Drawer';
+import {AvailableTabList} from "./components/AvailableTabList";
 
 function App() {
-  try {
-    initDB(DBConfig);
-  } catch (e) {
-    console.log(e);
-  }
-  const [open, setOpen] = useState(false);
+    try {
+        initDB(DBConfig);
+    } catch (e) {
+        console.log(e);
+    }
+    const [open, setOpen] = useState(false);
 
-  const openTabNameModal = () => {
-    setOpen(true);
-  };
+    const openTabNameModal = () => {
+        setOpen(true);
+    };
 
-  return (
-    <div>
-      <ErrorBoundary>
-        <Header />
-        <div className="app-wrapper container">
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Card variant="outlined">
-                <JsonTab openRenameTab={openTabNameModal} />
-                <JSONEditor />
-                <TabNameDialog open={open} setOpen={setOpen} />
-              </Card>
-            </Grid>
-          </Grid>
+    const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleTabs = ()=> {
+        console.log('opeining')
+        setDrawerOpen(!isDrawerOpen);
+    }
+
+    return (
+        <div>
+            <ErrorBoundary>
+                <Header toggleTabs={toggleTabs}/>
+                <div className="app-wrapper container">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Card variant="outlined">
+                                <JsonTab openRenameTab={openTabNameModal}/>
+                                <JSONEditor/>
+                                <TabNameDialog open={open} setOpen={setOpen}/>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                    <Drawer
+                        anchor='left'
+                        open={isDrawerOpen}
+                        onClose={() => setDrawerOpen(false)}
+                    >
+                        <AvailableTabList/>
+                    </Drawer>
+                </div>
+                <Footer/>
+            </ErrorBoundary>
         </div>
-        <Footer />
-      </ErrorBoundary>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import Button from '@mui/material/Button';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
-export function Header() {
+export function Header({toggleTabs}: props) {
 
     return (
         <div className="container">
             <header className="header-wrapper">
                 <h2>JSONIFY</h2>
-                <a href="https://github.com/sanjusudheerm/jsonify" target="_blank" className="link">
-                    <GitHubIcon/>
-                </a>
+                <div className="menu">
+                    <span onClick={toggleTabs}>Tabs</span>
+                    <a href="https://github.com/sanjusudheerm/jsonify" target="_blank" className="link">
+                        <GitHubIcon/>
+                    </a>
+                </div>
             </header>
         </div>
     );
